19b460eee997a1ffa7d445dfa1e0280aaf15494d
Muhammad Fahad <muhammad.fahad@ucalgary.ca>
2025-09-16 14:56:43 -0600

    can now get user input from cla and fixed missing int in function parameters

--- mochi.c
+++ mochi.c
@@ -1,16 +1,20 @@
+/*
+    CPSC 355 Assignment 1 - Hungry Mochi
+    Full Name: Muhammad Fahad
+    UCID: 30177705
+    LEC: LEC01
+    TUT: T02
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
-
+// -------------- Functions ----------------------
 
 // Create a grid Nrow x Ncol where N is taken from cla
 // Populate the grid with random integers from 0 to 9
-void initGrid(int N, forest[N][N]) {
+void initGrid(int N, int forest[N][N]) {
     srand(time(NULL));
 
     for (int row = 0; row < N; row++) {
@@ -21,11 +25,32 @@ void initGrid(int N, forest[N][N]) {
 }
 
 // Print the grid to the console
-void printGrid(int N, forest[N][N]) {
+void printGrid(int N, int forest[N][N]) {
     for (int row = 0; row < N; row++) {
         for (int col = 0; col < N; col++) {
             printf("%d ", forest[row][col]);
         }
         printf("\n");
     }
+}
+
+// -------------- Main ----------------------
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Please enter size of Forest Grid (N)");
+        return 0;
+    }
+
+    int N = atoi(argv[1]);
+    int forest[N][N];
+
+    initGrid(N,forest);
+
+    printf("<======= Mochi the Panda =======>\n");
+
+    printf("Generated Forest Grid:\n");
+    printGrid(N,forest);
+
+    return 0;
 }
